49dbe6df6bafbe1f8a78e9abb1fd78edf45c55db
Timothy Putzke <tsputzke@gmail.com>
2019-08-08 15:33:03 -0400

    first commit

---
+++ test/setup.js
@@ -0,0 +1,7 @@
+const { expect } = require('chai')
+const supertest = require('supertest')
+
+global.expect = expect
+global.supertest = supertest
+
+//As you progress with libraries such as chai, you might want to install plugins that add features to chai, this file would be the ideal place to do that.
